f707597c22e63aa4ef3fc892cc488ada1a4e6812
krdsstudent <krdsstudent@gmail.com>
2026-05-11 12:52:56 +0600

    Uncomment print statement and add new print line

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(
 )
 
 #print("Hello World")
+#print("World")
 print("Hello World")
 class TrackerApp(QtWidgets.QWidget):
     def __init__(self):
